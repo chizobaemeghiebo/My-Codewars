75d161c8bc0a366dcb5c6e6f177c271f33f8f5ca
Emeghiebo Chizoba <chizobaemeghiebo@gmail.com>
2022-09-07 16:30:29 +0100

    returns the thirs angle of a triangle

---
+++ 8kyus/Third-angle-of-a-triangle.js
@@ -0,0 +1,12 @@
+// INSTRUCTIONS
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+// solution
+function otherAngle(a, b) {
+	let twoAngles = a + b
+	return 180 - twoAngles
+}
