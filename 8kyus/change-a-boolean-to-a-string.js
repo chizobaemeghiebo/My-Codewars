326a760b81fca271ba9496aaa7666bb0d25b504a
Emeghiebo Chizoba <77364360+chizoba-009@users.noreply.github.com>
2022-09-10 11:38:38 +0100

    Create change-a-boolean-to-a-string.js

---
+++ 8kyus/change-a-boolean-to-a-string.js
@@ -0,0 +1,10 @@
+// INSTRUCTION
+
+// Implement a function which convert the given boolean value into its string representation.
+
+// SOLUTION
+
+function booleanToString(b) {
+	//your code here
+	return b.toString()
+}
