2ec345939c26cac2be8d6969a3ed0cff252c3c33
Emeghiebo Chizoba <77364360+chizoba-009@users.noreply.github.com>
2022-09-17 22:35:35 +0100

    for the love of green squares

---
+++ 8kyus/arrow-to-knee.js
@@ -0,0 +1,8 @@
+// INSTRUCTION
+
+// Come here to practice the Arrow style functions Not much else to say good luck!
+
+// SOLUTION
+var ArrowFunc = function (arr) {
+	return arr.map((e) => String.fromCharCode(e)).join('') //Complete this function
+}
