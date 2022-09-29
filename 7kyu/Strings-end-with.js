18dfebc48a23a69c29d211646836bcfe38775984
Emeghiebo Chizoba <77364360+chizoba-009@users.noreply.github.com>
2022-09-29 20:59:38 +0100

    another one

---
+++ 7kyu/Strings-end-with.js
@@ -0,0 +1,10 @@
+// INSTRUCTION
+
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// SOLUTION
+
+function solution(str, ending) {
+	// TODO: complete
+	return str.endsWith(ending)
+}
