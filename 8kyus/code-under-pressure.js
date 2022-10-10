792d0ebdea8e3350ba5d8f46112c5e22bcf289a1
Emeghiebo Chizoba <77364360+chizoba-009@users.noreply.github.com>
2022-10-10 22:31:01 +0100

    new kata

---
+++ 8kyus/code-under-pressure.js
@@ -0,0 +1,8 @@
+// INSTRUCTION
+// Code as fast as you can! You need to double the integer and return it.
+
+// SOLUTION
+function doubleInteger(i) {
+	// i will be an integer. Double it and return it.
+	return i + i
+}
