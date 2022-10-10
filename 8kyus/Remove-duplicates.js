53863c043af7ec0894e68aba0e0facbeb2a4905e
Emeghiebo Chizoba <77364360+chizoba-009@users.noreply.github.com>
2022-10-10 22:46:28 +0100

    removes duplicates from an array

---
+++ 8kyus/Remove-duplicates.js
@@ -0,0 +1,13 @@
+// INSTRUCTION
+
+// function distinct(a) {
+//   let noDuplicate = [... new Set(a)]
+//   return noDuplicate
+// }
+
+// SOLUTION
+
+function distinct(a) {
+	let noDuplicate = [...new Set(a)]
+	return noDuplicate
+}
