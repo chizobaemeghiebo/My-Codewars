594564a0017d17d967e7860742fc0f064c414e97
Emeghiebo Chizoba <77364360+chizoba-009@users.noreply.github.com>
2023-01-04 23:12:48 +0100

    converts binary to decimal

---
+++ 8kyus/Binary-to-decimal.js
@@ -0,0 +1,8 @@
+// INSTRUCTION
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+// SOLUTION
+function binToDec(bin) {
+  const converted = parseInt(bin, 2)
+  return converted
+}
